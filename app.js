f923369a4b7421e884fd45c6bc870ef994ac2b90
Cleophas <cleophasfoy@gmail.com>
2022-09-15 11:43:24 -0500

    added some movement to pacman

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
   ]
+
+  const squares = []
 //Legend
 //0 - pac-doctype
 //1 - wall
@@ -40,5 +42,56 @@ document.addEventListener('DOMContentLoaded', () => {
 //4 - empty
 
 //draw the grid and render it
+function createBoard() {
+  for (let i = 0; i <layout.length; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+    squares.push(square)
+
+    //add layout to the board
+    if(layout[i] === 0) {
+      squares[i].classList.add('pac-dot')
+    } else if (layout[i] === 1) {
+      squares[i].classList.add('wall')
+    } else if (layout[i] === 3) {
+      squares[i].classList.add('power-pellet')
+    }
+  }
+}
+
+createBoard()
+
+//starting position of pacman
+let pacmanCurrentIndex = 490
+
+squares[pacmanCurrentIndex].classList.add('pac-man')
+
+//move pac-man
+function movePacman(e) {
+  squares[pacmanCurrentIndex].classList.remove('pac-man')
+
+  switch(e.keyCode) {
+    case 37:
+      if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
+      break
+    case 38:
+      if(pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
+      break
+    case 39:
+      if(pacmanCurrentIndex % width < width -1) pacmanCurrentIndex += 1
+      break
+    case 40:
+      if(pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+      break
+  }
+
+  squares[pacmanCurrentIndex].classList.add('pac-man')
+
+  //pacDotEaten()
+  //powerPellet()
+  //checkForGameOver()
+
+}
+document.addEventListener('keyup', movePacman)
 
 })
